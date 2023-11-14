db35050d0d1104d697c7428defdcb5776cb43d0b
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2023-11-14 16:52:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,39 +3,27 @@ const path = require('path')
 
 const PORT = process.env.PORT || 5001
 const http = require('http');
+const bodyParser = require('body-parser');
 
 
-const server = http.createServer((req, res) => {
-  // Check if the request is for the /body endpoint
-  if (req.url === '/body' && req.method === 'POST') {
-    let body = '';
+// Parse incoming JSON requests
+app.use(bodyParser.json());
 
-    // Collect data chunks from the request
-    req.on('data', (chunk) => {
-      body += chunk.toString();
+// Endpoint for handling POST requests to /body
+app.post('/body', (req, res) => {
+  const body = req.body;
+
+  // Assuming Syslog messages are in the request body
+  try {
+    const syslogMessages = parseSyslogMessages(body);
+    console.log('Received Syslog messages:');
+    syslogMessages.forEach((message, index) => {
+      console.log(`Message ${index + 1}: ${message}`);
     });
-
-    // Process the request once it's complete
-    req.on('end', () => {
-      // Assuming Syslog messages are in the request body
-      try {
-        const syslogMessages = parseSyslogMessages(body);
-        console.log('Received Syslog messages:');
-        syslogMessages.forEach((message, index) => {
-          console.log(`Message ${index + 1}: ${message}`);
-        });
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Syslog messages processed successfully.');
-      } catch (error) {
-        console.error('Error processing Syslog messages:', error.message);
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
-      }
-    });
-  } else {
-    // Handle other endpoints or methods
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
+    res.status(200).send('Syslog messages processed successfully.');
+  } catch (error) {
+    console.error('Error processing Syslog messages:', error.message);
+    res.status(500).send('Internal Server Error');
   }
 });
 
@@ -45,6 +33,7 @@ function parseSyslogMessages(syslogData) {
   // For simplicity, let's assume each line in the body is a separate Syslog message
   return syslogData.split('\n').filter((message) => message.trim() !== '');
 }
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))

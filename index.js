5bec34b4fda5dc54f93768b6c4d43e4a9d89c296
kvandavasi <100835776+kvandavasi@users.noreply.github.com>
2023-11-14 17:07:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser'); // Require body-parser for parsing POST request bodies
+const winston = require('winston');
 
 const PORT = process.env.PORT || 5001;
 
+const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.json() // This will format the log message as JSON
+  ),
+  transports: [new winston.transports.Console()],
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.urlencoded({ extended: true })) // Use body-parser middleware for parsing URL-encoded bodies
+  .use(express.urlencoded({ extended: true }))
+  .use(express.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .post('/body', (req, res) => {
-    // Access the request body and print it
-    console.log('Request Body:', req.body);
-    res.send('Request Body Printed in Console'); // Send a response to the client
+    // Log the request body in syslog format
+    logger.info({
+      message: 'Request Body Received',
+      requestBody: req.body,
+      // You can add more fields like timestamp, hostname, etc. as needed
+    });
+
+    res.send('Request Body Logged in Console');
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
